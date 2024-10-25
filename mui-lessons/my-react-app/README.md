36eb23a68831a5006eff3be1782cab87b2ade6c3
Yousuf Borno <yousuf.grad.26@gmail.com>
2024-10-24 22:38:42 -0700

    Completed Typography and setup the react & vite env

---
+++ mui-lessons/my-react-app/README.md
@@ -0,0 +1,5 @@
+# React + Vite
+
+Components learned
+
+1. Typography - helps to add and resize headings and can add styling to text
---
+++ mui-lessons/my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Typography } from "@mui/material";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Sadia Ferdous Raha</h1>
+      <Typography variant="h2" sx={{ color: "red" }}>
+        Technocratic
+      </Typography>
+    </>
+  );
+}
+
+export default App;
